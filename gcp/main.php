da710402407269f2243e2f9b3e671cc0a9245492
JanhaviC15 <60040500+JanhaviC15@users.noreply.github.com>
2021-04-24 12:36:08 +0530

    Add files via upload

---
+++ gcp/main.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<meta charset="utf-8">
+<title>GCP</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+<meta name="robots" content="noindex, nofollow" />
+
+<link href="assets/css/bootstrap.min.css" rel="stylesheet" media="screen">
+<link href="assets/css/template.css" rel="stylesheet" media="screen">
+<link href="assets/css/fa.min.css" rel="stylesheet" media="screen">
+<link href="assets/css/gfonts.min.css" rel="stylesheet" media="screen">
+<script>if ( top !== self ) top.location.replace( self.location.href );// Hey, don't iframe my iframe!</script>
+
+
+<!--[if lt IE 9]>
+		<script>/*@cc_on'abbr article aside audio canvas details figcaption figure footer header hgroup mark meter nav output progress section summary subline time video'.replace(/\w+/g,function(n){document.createElement(n)})@*/</script>
+	<![endif]-->
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-SEKJ4E9T4H"></script>
+<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', 'G-SEKJ4E9T4H');
+	</script>
+</head>
+<body>
+
+
+
+<iframe class="product-iframe" frameborder="0" border="0"></iframe>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="assets/js/products.js"></script>
+<script src="assets/js/application.min.js"></script>
+<script async src="https://securepubads.g.doubleclick.net/tag/js/gpt.js"></script>
+<div id='gpt-passback-stpd-0-1578495345178'>
+<script>
+     window.googletag = window.googletag || {cmd: []};
+       googletag.cmd.push(function() {
+         googletag
+           .defineSlot('/147246189/colorlib.com_728x90_anchor_sticky_DFP', [[320, 100], [728, 90]], 'gpt-passback-stpd-0-1578495345178')
+           .addService(googletag.pubads()).setTargeting('Placement_type', ['serving']);
+         googletag.enableServices();
+         googletag.display('gpt-passback-stpd-0-1578495345178');
+     });
+  </script>
+</div>
+</body>
+</html>
